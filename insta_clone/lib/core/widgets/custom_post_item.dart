acd408d588cd860e5ca2e28a0c5e1c102edd67d7
HamzaLahmedi <hamzalahmedi07@gmail.com>
2024-05-26 18:47:44 +0100

    Add CustomPostItem widget and refactor HomeBodyView
    - Added CustomPostItem widget to display individual posts
    - Refactored HomeBodyView to use CustomPostItem widget for each post
    - Added intl package for date formatting

---
+++ insta_clone/lib/core/widgets/custom_post_item.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+import 'package:insta_clone/core/utils/colors.dart';
+import 'package:insta_clone/views/responsive/mobile/screens/widgets/bottom_post_icons.dart';
+import 'package:insta_clone/views/responsive/mobile/screens/widgets/home_body_header.dart';
+import 'package:intl/intl.dart';
+
+class CustomPostItem extends StatefulWidget {
+  const CustomPostItem({super.key, required this.data});
+final Map data;
+  @override
+  State<CustomPostItem> createState() => _CustomPostItemState();
+}
+
+class _CustomPostItemState extends State<CustomPostItem> {
+  @override
+  Widget build(BuildContext context) {
+    final double screenWidth = MediaQuery.of(context).size.width;
+    return Container(
+              margin: EdgeInsets.symmetric(
+                  vertical: screenWidth > 600 ? 55 : 0,
+                  horizontal: screenWidth > 600 ? screenWidth / 6 : 0),
+              decoration: BoxDecoration(
+                color: mobileBackgroundColor,
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: Column(
+                children: [
+                  HomeBodyHeader(
+                    imgUrl: widget.data['profileImg'] ?? '',
+                    userName: widget.data['username'] ?? 'null',
+                  ),
+                  Image.network(
+                    widget.data['imgPost'],
+                    fit: BoxFit.cover,
+                    height: MediaQuery.of(context).size.height * 0.35,
+                    width: double.infinity,
+                  ),
+                  const BottomPostIcons(),
+                  Container(
+                    margin: const EdgeInsets.fromLTRB(10, 0, 0, 10),
+                    width: double.infinity,
+                    child: Text(
+                      widget.data['likes'].length > 1
+                          ? " ${widget.data['likes'].length.toString()} likes"
+                          : "${widget.data['likes'].length.toString()} like",
+                      textAlign: TextAlign.start,
+                      style: const TextStyle(
+                        fontSize: 18,
+                        color: Color.fromARGB(214, 157, 157, 165),
+                      ),
+                    ),
+                  ),
+                  Row(
+                    children: [
+                      const SizedBox(
+                        width: 9,
+                      ),
+                      Text(
+                        widget.data['username'],
+                        textAlign: TextAlign.start,
+                        style: const TextStyle(
+                          fontSize: 20,
+                          color: Color.fromARGB(255, 189, 196, 199),
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 9,
+                      ),
+                      Text(
+                        // " ${widget.snap["description"]}",
+                        widget.data['description'],
+                        textAlign: TextAlign.start,
+                        style: const TextStyle(
+                          fontSize: 18,
+                          color: Color.fromARGB(255, 189, 196, 199),
+                        ),
+                      ),
+                    ],
+                  ),
+                  GestureDetector(
+                    onTap: () {},
+                    child: Container(
+                        margin: const EdgeInsets.fromLTRB(10, 13, 9, 10),
+                        width: double.infinity,
+                        child: const Text(
+                          "view all 100 comments",
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Color.fromARGB(214, 157, 157, 165),
+                          ),
+                          textAlign: TextAlign.start,
+                        )),
+                  ),
+                  Container(
+                      margin: const EdgeInsets.fromLTRB(10, 0, 9, 10),
+                      width: double.infinity,
+                      child: Text(
+                        DateFormat('yMMMd')
+                            .format(widget.data['datePublished'].toDate()),
+                        style: const TextStyle(
+                          fontSize: 18,
+                          color: Color.fromARGB(214, 157, 157, 165),
+                        ),
+                        textAlign: TextAlign.start,
+                      )),
+                ],
+              ),
+            );
+  }
+}
--- insta_clone/lib/views/responsive/mobile/screens/widgets/home_body_view.dart
+++ insta_clone/lib/views/responsive/mobile/screens/widgets/home_body_view.dart
@@ -1,8 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:insta_clone/core/utils/colors.dart';
+import 'package:insta_clone/core/widgets/custom_post_item.dart';
 import 'package:insta_clone/views/responsive/mobile/screens/widgets/bottom_post_icons.dart';
 import 'package:insta_clone/views/responsive/mobile/screens/widgets/home_body_header.dart';
+import 'package:intl/intl.dart';
 
 class HomeBodyView extends StatelessWidget {
   const HomeBodyView({
@@ -27,93 +29,8 @@ class HomeBodyView extends StatelessWidget {
           children: snapshot.data!.docs.map((DocumentSnapshot document) {
             Map<String, dynamic> data =
                 document.data()! as Map<String, dynamic>;
-            return Container(
-              margin: EdgeInsets.symmetric(
-                  vertical: screenWidth > 600 ? 55 : 0,
-                  horizontal: screenWidth > 600 ? screenWidth / 6 : 0),
-              decoration: BoxDecoration(
-                color: mobileBackgroundColor,
-                borderRadius: BorderRadius.circular(12),
-              ),
-              child: Column(
-                children: [
-                  HomeBodyHeader(
-                    imgUrl: data['profileImg'] ?? '',
-                    userName: data['username'] ?? 'null',
-                  ),
-                  Image.network(
-                    data['imgPost'],
-                    fit: BoxFit.cover,
-                    height: MediaQuery.of(context).size.height * 0.35,
-                    width: double.infinity,
-                  ),
-                  const BottomPostIcons(),
-                  Container(
-                    margin: const EdgeInsets.fromLTRB(10, 0, 0, 10),
-                    width: double.infinity,
-                    child: Text(
-                      "${data['likes'].length.toString()} likes",
-                      textAlign: TextAlign.start,
-                      style: const TextStyle(
-                        fontSize: 18,
-                        color: Color.fromARGB(214, 157, 157, 165),
-                      ),
-                    ),
-                  ),
-                  Row(
-                    children: [
-                      const SizedBox(
-                        width: 9,
-                      ),
-                      Text(
-                        data['username'],
-                        textAlign: TextAlign.start,
-                        style: const TextStyle(
-                          fontSize: 20,
-                          color: Color.fromARGB(255, 189, 196, 199),
-                        ),
-                      ),
-                      const SizedBox(
-                        width: 9,
-                      ),
-                      Text(
-                        // " ${widget.snap["description"]}",
-                        data['description'],
-                        textAlign: TextAlign.start,
-                        style: const TextStyle(
-                          fontSize: 18,
-                          color: Color.fromARGB(255, 189, 196, 199),
-                        ),
-                      ),
-                    ],
-                  ),
-                  GestureDetector(
-                    onTap: () {},
-                    child: Container(
-                        margin: const EdgeInsets.fromLTRB(10, 13, 9, 10),
-                        width: double.infinity,
-                        child: const Text(
-                          "view all 100 comments",
-                          style: TextStyle(
-                            fontSize: 18,
-                            color: Color.fromARGB(214, 157, 157, 165),
-                          ),
-                          textAlign: TextAlign.start,
-                        )),
-                  ),
-                  Container(
-                      margin: const EdgeInsets.fromLTRB(10, 0, 9, 10),
-                      width: double.infinity,
-                      child: const Text(
-                        '10 june 2024',
-                        style: TextStyle(
-                          fontSize: 18,
-                          color: Color.fromARGB(214, 157, 157, 165),
-                        ),
-                        textAlign: TextAlign.start,
-                      )),
-                ],
-              ),
+            return CustomPostItem(
+              data: data,
             );
           }).toList(),
         );
