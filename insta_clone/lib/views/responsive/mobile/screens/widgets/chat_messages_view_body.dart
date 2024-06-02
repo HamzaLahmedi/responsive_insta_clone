b1332d1e0526bd33f42bc04b19a05dd7290b1468
HamzaLahmedi <hamzalahmedi07@gmail.com>
2024-06-02 13:12:57 +0100

    adding chat feature to app ui and logic done 100%

--- insta_clone/lib/views/responsive/mobile/screens/widgets/chat_messages_view_body.dart
+++ insta_clone/lib/views/responsive/mobile/screens/widgets/chat_messages_view_body.dart
@@ -66,49 +66,53 @@ class _ChatMessagesViewBodyState extends State<ChatMessagesViewBody> {
                       print('data ****************** $data');
                       bool isSender = data['senderId'] == currentUserId;
 
-                      return Align(
-                        alignment: isSender
-                            ? Alignment.centerRight
-                            : Alignment.centerLeft,
-                        child: Row(
-                          mainAxisAlignment: isSender
-                              ? MainAxisAlignment.end
-                              : MainAxisAlignment.start,
-                          children: [
-                            if (!isSender)
-                              CircleAvatar(
-                                backgroundImage:
-                                    NetworkImage(data['senderImg']),
-                                radius: 20,
-                              ),
-                            const SizedBox(width: 8),
-                            Container(
-                              padding: const EdgeInsets.all(12),
-                              decoration: BoxDecoration(
-                                color: isSender
-                                    ? Colors.deepPurple
-                                    : Colors.grey[300],
-                                borderRadius: BorderRadius.circular(8),
-                              ),
-                              constraints: BoxConstraints(
-                                maxWidth:
-                                    MediaQuery.of(context).size.width * 0.75,
-                              ),
-                              child: Text(
-                                data['message'],
-                                style: TextStyle(
-                                  color: isSender ? Colors.white : Colors.black,
+                      return Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Align(
+                          alignment: isSender
+                              ? Alignment.centerRight
+                              : Alignment.centerLeft,
+                          child: Row(
+                            mainAxisAlignment: isSender
+                                ? MainAxisAlignment.end
+                                : MainAxisAlignment.start,
+                            children: [
+                              if (!isSender)
+                                CircleAvatar(
+                                  backgroundImage:
+                                      NetworkImage(data['senderImg']),
+                                  radius: 20,
+                                ),
+                              const SizedBox(width: 8),
+                              Container(
+                                padding: const EdgeInsets.all(12),
+                                decoration: BoxDecoration(
+                                  color: isSender
+                                      ? Colors.deepPurple
+                                      : Colors.grey[300],
+                                  borderRadius: BorderRadius.circular(8),
+                                ),
+                                constraints: BoxConstraints(
+                                  maxWidth:
+                                      MediaQuery.of(context).size.width * 0.75,
+                                ),
+                                child: Text(
+                                  data['message'],
+                                  style: TextStyle(
+                                    color:
+                                        isSender ? Colors.white : Colors.black,
+                                  ),
                                 ),
                               ),
-                            ),
-                            const SizedBox(width: 8),
-                            if (isSender)
-                              CircleAvatar(
-                                backgroundImage:
-                                    NetworkImage(data['senderImg']),
-                                radius: 20,
-                              ),
-                          ],
+                              const SizedBox(width: 8),
+                              if (isSender)
+                                CircleAvatar(
+                                  backgroundImage:
+                                      NetworkImage(data['senderImg']),
+                                  radius: 20,
+                                ),
+                            ],
+                          ),
                         ),
                       );
                     }).toList(),
